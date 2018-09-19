63a3d7e411f963b737d332b9669b2990afe3db00
Jeff Newcomer <jrnewk@gmail.com>
2018-09-19 11:33:05 -0700

    Added title, carousel images, and arrows #1

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import $ from 'jquery'
 
-console.log('hello jeff')
+$(document).ready(() => {
+  let currentPic = 1;
+  const picCount = $('.img-box').length;
+  $('#next').click(() => {
+    if (currentPic >= picCount) {
+      return null
+    }
+    else {
+      currentPic++
+      $('.img-box').addClass('hidden')
+      $(`div[data-link=${currentPic}]`).removeClass('hidden')
+    }
+  })
+  $('#prev').click(() => {
+    if (currentPic <= 1) {
+      return null
+    }
+    else {
+      currentPic--
+      $('.img-box').addClass('hidden')
+      $(`div[data-link=${currentPic}]`).removeClass('hidden')
+    }
+  })
+})
