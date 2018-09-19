cc231390ec8aa6aa44ae2cc43b93675631d2e96d
Jeff Newcomer <jrnewk@gmail.com>
2018-09-19 12:12:41 -0700

    Added clickable dots below carousel which are also responsive to arrow clicks. #3

--- index.js
+++ index.js
@@ -3,24 +3,47 @@ import $ from 'jquery'
 $(document).ready(() => {
   let currentPic = 1;
   const picCount = $('.img-box').length;
+
+  $('.dotstyle a').on('click', function () {
+    $('.dotstyle li').removeClass('current')
+    $(this).parent('li').addClass('current')
+    const picNum = ($(this).parent('li').attr('data-num'));
+    currentPic = picNum
+    $('.img-box').addClass('hidden')
+    $(`div[data-link=${picNum}]`).removeClass('hidden')
+  })
+
   $('#next').click(() => {
     if (currentPic >= picCount) {
-      return null
+      currentPic = 1
+      $('.img-box').addClass('hidden')
+      $(`div[data-link=${currentPic}]`).removeClass('hidden')
+      $('.dotstyle li').removeClass('current')
+      $(`li[data-num=${currentPic}]`).addClass('current')
     }
     else {
       currentPic++
       $('.img-box').addClass('hidden')
       $(`div[data-link=${currentPic}]`).removeClass('hidden')
+      $('.dotstyle li').removeClass('current')
+      $(`li[data-num=${currentPic}]`).addClass('current')
     }
   })
   $('#prev').click(() => {
     if (currentPic <= 1) {
-      return null
+      currentPic = picCount
+      $('.img-box').addClass('hidden')
+      $(`div[data-link=${currentPic}]`).removeClass('hidden')
+      $('.dotstyle li').removeClass('current')
+      $(`li[data-num=${currentPic}]`).addClass('current')
     }
     else {
       currentPic--
       $('.img-box').addClass('hidden')
       $(`div[data-link=${currentPic}]`).removeClass('hidden')
+      $('.dotstyle li').removeClass('current')
+      $(`li[data-num=${currentPic}]`).addClass('current')
     }
   })
+
 })
